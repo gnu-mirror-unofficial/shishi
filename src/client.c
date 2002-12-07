26f602cb2cf72d846969f7a27bf7fe56d3e2405f
Simon Josefsson <simon@josefsson.org>
2002-12-07 07:03:51 +0000

    Use new AP functions.

--- src/client.c
+++ src/client.c
@@ -21,90 +21,35 @@
 
 #include "data.h"
 
-#define SERVER_NAME "sample"
-
 int
 client (Shishi * handle, struct arguments arg)
 {
-  Shishi_ticket *tkt, *tmptkt;
+  Shishi_ticket *tkt;
   Shishi_ap *ap;
-  ASN1_TYPE apreq;
   int res;
 
   if (arg.cname == NULL)
     arg.cname = shishi_principal_default (handle);
 
-  if (arg.realm == NULL)
-    arg.realm = shishi_realm_default (handle);
-
-  if (arg.sname == NULL)
-    {
-      int len = strlen (SERVER_NAME "/") + strlen (arg.realm) + 1;
-      arg.sname = malloc (len);
-      if (arg.sname == NULL)
-	return SHISHI_MALLOC_ERROR;
-      sprintf (arg.sname, "%s/%s", SERVER_NAME, arg.realm);
-    }
-
-  if (arg.tgtname == NULL)
-    {
-      int len = strlen ("krbtgt/") + strlen (arg.realm) + 1;
-      arg.tgtname = malloc (len);
-      if (arg.tgtname == NULL)
-	return SHISHI_MALLOC_ERROR;
-      sprintf (arg.tgtname, "krbtgt/%s", arg.realm);
-    }
-
   if (arg.verbose)
-    {
-      printf ("Client name: `%s'\n", arg.cname);
-      printf ("Realm: `%s'\n", arg.realm);
-      printf ("Ticket granter: `%s'\n", arg.tgtname);
-      printf ("Service name: `%s'\n", arg.sname);
-    }
+    printf ("Service name: `%s'\n", arg.sname);
 
-  tkt = shishi_ticketset_find_ticket_for_clientserver (handle, NULL,
-						       arg.cname, arg.sname);
+  tkt = shishi_ticketset_get_ticket_for_server (handle, NULL, arg.sname);
   if (tkt == NULL)
     {
-      ASN1_TYPE req, rep;
-
-      tkt = shishi_ticketset_find_ticket_for_clientserver (handle, NULL,
-							   arg.cname,
-							   arg.tgtname);
-      if (tkt == NULL)
-	req = shishi_asreq (handle, arg.realm, arg.sname, arg.cname);
-      else
-	req = shishi_tgsreq (handle, arg.realm, arg.sname, tkt);
-
-      res = shishi_kdcreq_sendrecv (handle, req, &rep);
-      if (res != SHISHI_OK)
-	{
-	  printf ("Could not send to KDC: %s\n", shishi_strerror (res));
-	  return res;
-	}
-
-      res = kdc_response (handle, arg, req, rep, tkt, &tmptkt);
-      if (res != 0)
-	return res;
-
-      res = shishi_ticketset_add (handle, NULL, tmptkt);
-      if (res != SHISHI_OK)
-	{
-	  printf ("Could not add ticket: %s", shishi_strerror (res));
-	  return res;
-	}
-      tkt = tmptkt;
-    }
-
-  res = shishi_ticket_apreq_data (handle, tkt, NULL, 0, &apreq);
-  if (res != SHISHI_OK)
-    {
-      printf ("Could not create AP-REQ: %s", shishi_strerror (res));
+      printf ("Cannot get ticket for server `%s'.\n", arg.sname);
       return res;
     }
 
-  res = shishi_apreq_options_set (handle, apreq, arg.apoptions);
+  res = shishi_ap_data (handle, tkt, NULL, 0, &ap);
+  if (res != SHISHI_OK)
+    {
+      printf ("Could not create AP: %s", shishi_strerror (res));
+      return res;
+    }
+
+  res = shishi_apreq_options_set (handle, shishi_ap_get_apreq(ap),
+				  arg.apoptions);
   if (res != SHISHI_OK)
     {
       printf ("Could not set AP-Options: %s", shishi_strerror (res));
@@ -113,9 +58,26 @@ client (Shishi * handle, struct arguments arg)
 
   if (arg.verbose)
     shishi_authenticator_print (handle, stdout,
-				shishi_last_authenticator (handle));
+				shishi_ap_get_authenticator(ap));
 
-  shishi_apreq_print (handle, stdout, apreq);
+  shishi_apreq_print (handle, stdout, shishi_ap_get_apreq(ap));
+
+  if (shishi_apreq_mutual_required_p (handle, shishi_ap_get_apreq(ap)))
+    {
+      ASN1_TYPE aprep;
+
+      printf ("Waiting for AP-REP from server...\n");
+
+      res = shishi_aprep_parse (handle, stdin, &aprep);
+
+      res = shishi_ap_reply_verify (handle, ap, aprep);
+      if (res == SHISHI_APREP_VERIFY_FAILED)
+	printf("AP-REP verification failed...\n");
+      else if (res == SHISHI_OK)
+	printf("AP-REP verification OK...\n");
+      else
+	printf("AP-REP verification error: %s\n", shishi_strerror(res));
+    }
 
   return SHISHI_OK;
 }
