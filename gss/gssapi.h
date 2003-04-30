fb65b673f3718f719afd8758efce8bbb907e91a7
Simon Josefsson <simon@josefsson.org>
2003-04-30 22:59:34 +0000

    Last commit was corrupt. Never use xom.h. Define gss_ctx_id_t, gss_cred_id_t, gss_name_t, and gss_uint32. Fix routine errors.

--- gss/gssapi.h
+++ gss/gssapi.h
@@ -8,40 +8,27 @@
  */
 #include <stddef.h>
 
-/*
- * If the platform supports the xom.h header file, it should be
- * included here.
- */
-#include <xom.h>
-
 
 /*
  * Now define the three implementation-dependent types.
  */
-typedef <platform-specific> gss_ctx_id_t;
-typedef <platform-specific> gss_cred_id_t;
-typedef <platform-specific> gss_name_t;
+typedef struct gss_ctx_id_t  *gss_ctx_id_t;
+typedef struct gss_cred_id_t *gss_cred_id_t;
+typedef struct gss_name_t    *gss_name_t;
 
 /*
  * The following type must be defined as the smallest natural
  * unsigned integer supported by the platform that has at least
  * 32 bits of precision.
  */
-typedef <platform-specific> gss_uint32;
-
-
-#ifdef OM_STRING
-/*
- * We have included the xom.h header file.  Verify that OM_uint32
- * is defined correctly.
- */
-
-#if sizeof(gss_uint32) != sizeof(OM_uint32)
-#error Incompatible definition of OM_uint32 from xom.h
+#include <limits.h>
+#if USHRT_MAX >= 4294967295
+typedef unsigned short gss_uint32;
+#elif UINT_MAX >= 4294967295
+typedef unsigned int gss_uint32;
+#else /* unsigned long's must be at least 32 bits according to K&R */
+typedef unsigned long gss_uint32;
 #endif
-
-typedef OM_object_identifier gss_OID_desc, *gss_OID;
-#else
 
 /*
  * We can't use X/Open definitions, so roll our own.
@@ -54,7 +41,6 @@ typedef struct gss_OID_desc_struct {
   void      *elements;
 } gss_OID_desc, *gss_OID;
 
-#endif
 
 typedef struct gss_OID_set_desc_struct  {
   size_t     count;
@@ -171,7 +157,326 @@ typedef int gss_cred_usage_t;
 
 /*
  * Expiration time of 2^32-1 seconds means infinite lifetime for a
- * cred
+ * credential or security context
+ */
+#define GSS_C_INDEFINITE 0xfffffffful
+
+/*
+ * The implementation must reserve static storage for a
+ * gss_OID_desc object containing the value
+ * {10, (void *)"\x2a\x86\x48\x86\xf7\x12"
+ * "\x01\x02\x01\x01"},
+ * corresponding to an object-identifier value of
+ * {iso(1) member-body(2) United States(840) mit(113554)
+ * infosys(1) gssapi(2) generic(1) user_name(1)}.  The constant
+ * GSS_C_NT_USER_NAME should be initialized to point
+ * to that gss_OID_desc.
+ */
+extern gss_OID GSS_C_NT_USER_NAME;
+
+/*
+ * The implementation must reserve static storage for a
+ * gss_OID_desc object containing the value
+ * {10, (void *)"\x2a\x86\x48\x86\xf7\x12"
+ *              "\x01\x02\x01\x02"},
+ * corresponding to an object-identifier value of
+ * {iso(1) member-body(2) United States(840) mit(113554)
+ * infosys(1) gssapi(2) generic(1) machine_uid_name(2)}.
+ * The constant GSS_C_NT_MACHINE_UID_NAME should be
+ * initialized to point to that gss_OID_desc.
+ */
+extern gss_OID GSS_C_NT_MACHINE_UID_NAME;
+
+/*
+ * The implementation must reserve static storage for a
+ * gss_OID_desc object containing the value
+ * {10, (void *)"\x2a\x86\x48\x86\xf7\x12"
+ *              "\x01\x02\x01\x03"},
+ * corresponding to an object-identifier value of
+ * {iso(1) member-body(2) United States(840) mit(113554)
+ * infosys(1) gssapi(2) generic(1) string_uid_name(3)}.
+ * The constant GSS_C_NT_STRING_UID_NAME should be
+ * initialized to point to that gss_OID_desc.
+ */
+extern gss_OID GSS_C_NT_STRING_UID_NAME;
+
+/*
+ * The implementation must reserve static storage for a
+ * gss_OID_desc object containing the value
+ * {6, (void *)"\x2b\x06\x01\x05\x06\x02"},
+ * corresponding to an object-identifier value of
+ * {iso(1) org(3) dod(6) internet(1) security(5)
+ * nametypes(6) gss-host-based-services(2)).  The constant
+ * GSS_C_NT_HOSTBASED_SERVICE_X should be initialized to point
+ * to that gss_OID_desc.  This is a deprecated OID value, and
+ * implementations wishing to support hostbased-service names
+ * should instead use the GSS_C_NT_HOSTBASED_SERVICE OID,
+ * defined below, to identify such names;
+ * GSS_C_NT_HOSTBASED_SERVICE_X should be accepted a synonym
+ * for GSS_C_NT_HOSTBASED_SERVICE when presented as an input
+ * parameter, but should not be emitted by GSS-API
+ * implementations
+ */
+extern gss_OID GSS_C_NT_HOSTBASED_SERVICE_X;
+
+/*
+ * The implementation must reserve static storage for a
+ * gss_OID_desc object containing the value
+ * {10, (void *)"\x2a\x86\x48\x86\xf7\x12"
+ *              "\x01\x02\x01\x04"}, corresponding to an
+ * object-identifier value of {iso(1) member-body(2)
+ * Unites States(840) mit(113554) infosys(1) gssapi(2)
+ * generic(1) service_name(4)}.  The constant
+ * GSS_C_NT_HOSTBASED_SERVICE should be initialized
+ * to point to that gss_OID_desc.
+ */
+extern gss_OID GSS_C_NT_HOSTBASED_SERVICE;
+
+/*
+ * The implementation must reserve static storage for a
+ * gss_OID_desc object containing the value
+ * {6, (void *)"\x2b\x06\01\x05\x06\x03"},
+ * corresponding to an object identifier value of
+ * {1(iso), 3(org), 6(dod), 1(internet), 5(security),
+ * 6(nametypes), 3(gss-anonymous-name)}.  The constant
+ * and GSS_C_NT_ANONYMOUS should be initialized to point
+ * to that gss_OID_desc.
+ */
+extern gss_OID GSS_C_NT_ANONYMOUS;
+
+
+/*
+ * The implementation must reserve static storage for a
+ * gss_OID_desc object containing the value
+ * {6, (void *)"\x2b\x06\x01\x05\x06\x04"},
+ * corresponding to an object-identifier value of
+ * {1(iso), 3(org), 6(dod), 1(internet), 5(security),
+ * 6(nametypes), 4(gss-api-exported-name)}.  The constant
+ * GSS_C_NT_EXPORT_NAME should be initialized to point
+ * to that gss_OID_desc.
+ */
+extern gss_OID GSS_C_NT_EXPORT_NAME;
+
+
+/* Major status codes */
+
+#define GSS_S_COMPLETE 0
+
+/*
+ * Some "helper" definitions to make the status code macros obvious.
+ */
+#define GSS_C_CALLING_ERROR_OFFSET 24
+#define GSS_C_ROUTINE_ERROR_OFFSET 16
+#define GSS_C_SUPPLEMENTARY_OFFSET 0
+#define GSS_C_CALLING_ERROR_MASK 0377ul
+#define GSS_C_ROUTINE_ERROR_MASK 0377ul
+#define GSS_C_SUPPLEMENTARY_MASK 0177777ul
+
+/*
+ * The macros that test status codes for error conditions.
+ * Note that the GSS_ERROR() macro has changed slightly from
+ * the V1 GSS-API so that it now evaluates its argument
+ * only once.
+ */
+#define GSS_CALLING_ERROR(x) \
+ (x & (GSS_C_CALLING_ERROR_MASK << GSS_C_CALLING_ERROR_OFFSET))
+#define GSS_ROUTINE_ERROR(x) \
+ (x & (GSS_C_ROUTINE_ERROR_MASK << GSS_C_ROUTINE_ERROR_OFFSET))
+#define GSS_SUPPLEMENTARY_INFO(x) \
+ (x & (GSS_C_SUPPLEMENTARY_MASK << GSS_C_SUPPLEMENTARY_OFFSET))
+#define GSS_ERROR(x) \
+ (x & ((GSS_C_CALLING_ERROR_MASK << GSS_C_CALLING_ERROR_OFFSET) | \
+       (GSS_C_ROUTINE_ERROR_MASK << GSS_C_ROUTINE_ERROR_OFFSET)))
+
+/*
+ * Now the actual status code definitions
+ */
+
+/*
+ * Calling errors:
+
+ */
+#define GSS_S_CALL_INACCESSIBLE_READ \
+(1ul << GSS_C_CALLING_ERROR_OFFSET)
+#define GSS_S_CALL_INACCESSIBLE_WRITE \
+(2ul << GSS_C_CALLING_ERROR_OFFSET)
+#define GSS_S_CALL_BAD_STRUCTURE \
+(3ul << GSS_C_CALLING_ERROR_OFFSET)
+
+/*
+ * Routine errors:
+ */
+#define GSS_S_BAD_MECH             (1ul << GSS_C_ROUTINE_ERROR_OFFSET)
+#define GSS_S_BAD_NAME             (2ul << GSS_C_ROUTINE_ERROR_OFFSET)
+#define GSS_S_BAD_NAMETYPE         (3ul << GSS_C_ROUTINE_ERROR_OFFSET)
+#define GSS_S_BAD_BINDINGS         (4ul << GSS_C_ROUTINE_ERROR_OFFSET)
+#define GSS_S_BAD_STATUS           (5ul << GSS_C_ROUTINE_ERROR_OFFSET)
+#define GSS_S_BAD_SIG              (6ul << GSS_C_ROUTINE_ERROR_OFFSET)
+#define GSS_S_BAD_MIC GSS_S_BAD_SIG
+#define GSS_S_NO_CRED              (7ul << GSS_C_ROUTINE_ERROR_OFFSET)
+#define GSS_S_NO_CONTEXT           (8ul << GSS_C_ROUTINE_ERROR_OFFSET)
+#define GSS_S_DEFECTIVE_TOKEN      (9ul << GSS_C_ROUTINE_ERROR_OFFSET)
+#define GSS_S_DEFECTIVE_CREDENTIAL (10ul << GSS_C_ROUTINE_ERROR_OFFSET)
+#define GSS_S_CREDENTIALS_EXPIRED  (11ul << GSS_C_ROUTINE_ERROR_OFFSET)
+#define GSS_S_CONTEXT_EXPIRED      (12ul << GSS_C_ROUTINE_ERROR_OFFSET)
+#define GSS_S_FAILURE              (13ul << GSS_C_ROUTINE_ERROR_OFFSET)
+#define GSS_S_BAD_QOP              (14ul << GSS_C_ROUTINE_ERROR_OFFSET)
+#define GSS_S_UNAUTHORIZED         (15ul << GSS_C_ROUTINE_ERROR_OFFSET)
+#define GSS_S_UNAVAILABLE          (16ul << GSS_C_ROUTINE_ERROR_OFFSET)
+#define GSS_S_DUPLICATE_ELEMENT    (17ul << GSS_C_ROUTINE_ERROR_OFFSET)
+#define GSS_S_NAME_NOT_MN          (18ul << GSS_C_ROUTINE_ERROR_OFFSET)
+
+/*
+ * Supplementary info bits:
+ */
+#define GSS_S_CONTINUE_NEEDED \
+         (1ul << (GSS_C_SUPPLEMENTARY_OFFSET + 0))
+#define GSS_S_DUPLICATE_TOKEN \
+         (1ul << (GSS_C_SUPPLEMENTARY_OFFSET + 1))
+#define GSS_S_OLD_TOKEN \
+         (1ul << (GSS_C_SUPPLEMENTARY_OFFSET + 2))
+#define GSS_S_UNSEQ_TOKEN \
+         (1ul << (GSS_C_SUPPLEMENTARY_OFFSET + 3))
+#define GSS_S_GAP_TOKEN \
+         (1ul << (GSS_C_SUPPLEMENTARY_OFFSET + 4))
+
+/*
+ * Finally, function prototypes for the GSS-API routines.
+ */
+
+
+OM_uint32 gss_acquire_cred
+              (OM_uint32 ,             /*  minor_status */
+               const gss_name_t,       /* desired_name */
+               OM_uint32,              /* time_req */
+               const gss_OID_set,      /* desired_mechs */
+               gss_cred_usage_t,       /* cred_usage */
+               gss_cred_id_t ,         /* output_cred_handle */
+               gss_OID_set ,           /* actual_mechs */
+               OM_uint32 *             /* time_rec */
+              );
+
+OM_uint32 gss_release_cred
+              (OM_uint32 ,             /* minor_status */
+               gss_cred_id_t *         /* cred_handle */
+              );
+
+OM_uint32 gss_init_sec_context
+              (OM_uint32 ,             /* minor_status */
+               const gss_cred_id_t,    /* initiator_cred_handle */
+               gss_ctx_id_t ,          /* context_handle */
+               const gss_name_t,       /* target_name */
+               const gss_OID,          /* mech_type */
+               OM_uint32,              /* req_flags */
+               OM_uint32,              /* time_req */
+               const gss_channel_bindings_t,
+                                       /* input_chan_bindings */
+               const gss_buffer_t,     /* input_token */
+               gss_OID ,               /* actual_mech_type */
+               gss_buffer_t,           /* output_token */
+               OM_uint32 ,             /* ret_flags */
+               OM_uint32 *             /* time_rec */
+              );
+
+OM_uint32 gss_accept_sec_context
+              (OM_uint32 ,             /* minor_status */
+               gss_ctx_id_t ,          /* context_handle */
+               const gss_cred_id_t,    /* acceptor_cred_handle */
+               const gss_buffer_t,     /* input_token_buffer */
+               const gss_channel_bindings_t,
+                                       /* input_chan_bindings */
+               gss_name_t ,            /* src_name */
+               gss_OID ,               /* mech_type */
+               gss_buffer_t,           /* output_token */
+               OM_uint32 ,             /* ret_flags */
+               OM_uint32 ,             /* time_rec */
+               gss_cred_id_t *         /* delegated_cred_handle */
+              );
+
+OM_uint32 gss_process_context_token
+              (OM_uint32 ,             /* minor_status */
+               const gss_ctx_id_t,     /* context_handle */
+               const gss_buffer_t      /* token_buffer */
+              );
+
+OM_uint32 gss_delete_sec_context
+              (OM_uint32 ,             /* minor_status */
+               gss_ctx_id_t ,          /* context_handle */
+               gss_buffer_t            /* output_token */
+              );
+
+OM_uint32 gss_context_time
+              (OM_uint32 ,             /* minor_status */
+               const gss_ctx_id_t,     /* context_handle */
+               OM_uint32 *             /* time_rec */
+              );
+
+OM_uint32 gss_get_mic
+              (OM_uint32 ,             /* minor_status */
+               const gss_ctx_id_t,     /* context_handle */
+               gss_qop_t,              /* qop_req */
+               const gss_buffer_t,     /* message_buffer */
+               gss_buffer_t            /* message_token */
+              );
+
+OM_uint32 gss_verify_mic
+              (OM_uint32 ,             /* minor_status */
+               const gss_ctx_id_t,     /* context_handle */
+               const gss_buffer_t,     /* message_buffer */
+               const gss_buffer_t,     /* token_buffer */
+               gss_qop_t *             /* qop_state */
+              );
+
+OM_uint32 gss_wrap
+              (OM_uint32 ,             /* minor_status */
+               const gss_ctx_id_t,     /* context_handle */
+               int,                    /* conf_req_flag */
+               gss_qop_t,              /* qop_req */
+               const gss_buffer_t,     /* input_message_buffer */
+               int ,                   /* conf_state */
+               gss_buffer_t            /* output_message_buffer */
+              );
+
+
+
+
+
+OM_uint32 gss_unwrap
+              (OM_uint32 ,             /* minor_status */
+               const gss_ctx_id_t,     /* context_handle */
+               const gss_buffer_t,     /* input_message_buffer */
+               gss_buffer_t,           /* output_message_buffer */
+               int ,                   /* conf_state */
+               gss_qop_t *             /* qop_state */
+              );
+
+
+
+OM_uint32 gss_display_status
+              (OM_uint32 ,             /* minor_status */
+               OM_uint32,              /* status_value */
+               int,                    /* status_type */
+               const gss_OID,          /* mech_type */
+               OM_uint32 ,             /* message_context */
+               gss_buffer_t            /* status_string */
+              );
+
+OM_uint32 gss_indicate_mechs
+              (OM_uint32 ,             /* minor_status */
+               gss_OID_set *           /* mech_set */
+              );
+
+OM_uint32 gss_compare_name
+              (OM_uint32 ,             /* minor_status */
+               const gss_name_t,       /* name1 */
+               const gss_name_t,       /* name2 */
+               int *                   /* name_equal */
+              );
+
+OM_uint32 gss_display_name
+              (OM_uint32 ,             /* minor_status */
+               const gss_name_t,       /* input_name */
+               gss_buffer_t,           /* output_name_buffer */
                gss_OID *               /* output_name_type */
               );
 
