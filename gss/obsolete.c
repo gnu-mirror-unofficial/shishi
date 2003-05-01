168e78959ab9a974040d6f7caae01113e240461f
Simon Josefsson <simon@josefsson.org>
2003-05-01 00:55:14 +0000

    Warn obsolete applications.

--- gss/obsolete.c
+++ gss/obsolete.c
@@ -21,12 +21,16 @@
 
 #include "internal.h"
 
+#define WARN(msg) fprintf(stderr, "warning: " msg "\n");
+
 OM_uint32
 gss_sign (OM_uint32 * minor_status,
 	  gss_ctx_id_t context_handle,
 	  int qop_req,
 	  gss_buffer_t message_buffer, gss_buffer_t message_token)
 {
+  WARN ("gss_sign() is obsolete, use gss_get_mic() instead.");
+
   return gss_get_mic (minor_status, context_handle,
 		      qop_req, message_buffer, message_token);
 }
@@ -38,6 +42,8 @@ gss_verify (OM_uint32 * minor_status,
 	    gss_buffer_t message_buffer,
 	    gss_buffer_t token_buffer, int *qop_state)
 {
+  WARN ("gss_verify() is obsolete, use gss_verify_mic() instead.");
+
   return gss_verify_mic (minor_status, context_handle, message_buffer,
 			 token_buffer, qop_state);
 }
@@ -50,6 +56,8 @@ gss_seal (OM_uint32 * minor_status,
 	  gss_buffer_t input_message_buffer,
 	  int *conf_state, gss_buffer_t output_message_buffer)
 {
+  WARN ("gss_seal() is obsolete, use gss_wap() instead.");
+
   return gss_wrap (minor_status, context_handle, conf_req_flag, qop_req,
 		   input_message_buffer, conf_state, output_message_buffer);
 }
@@ -62,6 +70,8 @@ gss_unseal (OM_uint32 * minor_status,
 	    gss_buffer_t output_message_buffer,
 	    int *conf_state, int *qop_state)
 {
+  WARN ("gss_unseal() is obsolete, use gss_unwrap() instead.");
+
   return gss_unwrap (minor_status, context_handle, input_message_buffer,
 		     output_message_buffer, conf_state, qop_state);
 }
