84a6f66070258aa499a7db20fe7f92679c3ac714
Simon Josefsson <simon@josefsson.org>
2003-08-10 17:01:32 +0000

    Work.

--- crypto/cbc-cts.c
+++ crypto/cbc-cts.c
@@ -32,29 +32,27 @@
 #include <string.h>
 
 void
-cbc_cts_encrypt(void *ctx, void (*f)(void *ctx,
-				 unsigned length, uint8_t *dst,
-				 const uint8_t *src),
-	    unsigned block_size, uint8_t *iv,
-	    unsigned length, uint8_t *dst,
-	    const uint8_t *src)
+cbc_cts_encrypt(void *ctx,
+		void (*f)(void *ctx,
+			  unsigned length,
+			  uint8_t *dst,
+			  const uint8_t *src),
+		unsigned block_size, uint8_t *iv,
+		unsigned length, uint8_t *dst,
+		const uint8_t *src)
 {
-  unsigned olength = length;
+  unsigned nblocks = length / block_size;
   unsigned restbytes = (length % block_size) == 0 ?
     block_size : length % block_size;
 
-  if (olength > block_size)
-    length -= block_size;
-
-  for ( ; length >= block_size;
-	length -= block_size, src += block_size, dst += block_size)
+  for ( ; nblocks; nblocks--, src += block_size, dst += block_size)
     {
       memxor(iv, src, block_size);
       f(ctx, block_size, dst, iv);
       memcpy(iv, dst, block_size);
     }
 
-  if (olength > block_size)
+  if (length > block_size)
     {
       memcpy(dst, dst - block_size, restbytes);
       dst -= block_size;
@@ -65,85 +63,47 @@ cbc_cts_encrypt(void *ctx, void (*f)(void *ctx,
     }
 }
 
-/* Reqires that dst != src */
-static void
-cbc_cts_decrypt_internal(void *ctx, void (*f)(void *ctx,
-					  unsigned length, uint8_t *dst,
-					  const uint8_t *src),
-		     unsigned block_size, uint8_t *iv,
-		     unsigned length, uint8_t *dst,
-		     const uint8_t *src)
-{
-  assert(length);
-  assert( !(length % block_size) );
-  assert(src != dst);
-
-  /* Decrypt in ECB mode */
-  f(ctx, length, dst, src);
-
-  /* XOR the cryptotext, shifted one block */
-  memxor(dst, iv, block_size);
-  memxor(dst + block_size, src, length - block_size);
-  memcpy(iv, src + length - block_size, block_size);
-}
-
-/* Don't allocate any more space than this on the stack */
-#define CBC_CTS_BUFFER_LIMIT 4096
-
 void
-cbc_cts_decrypt(void *ctx, void (*f)(void *ctx,
-				 unsigned length, uint8_t *dst,
-				 const uint8_t *src),
-	    unsigned block_size, uint8_t *iv,
-	    unsigned length, uint8_t *dst,
-	    const uint8_t *src)
+cbc_cts_decrypt(void *ctx,
+		void (*f)(void *ctx,
+			  unsigned length,
+			  uint8_t *dst,
+			  const uint8_t *src),
+		unsigned block_size, uint8_t *iv,
+		unsigned length, uint8_t *dst,
+		const uint8_t *src)
 {
-  assert(!(length % block_size));
+  unsigned nblocks = length / block_size;
+  unsigned restbytes = (length % block_size) == 0 ?
+    block_size : length % block_size;
+  char *tmpiv = alloca (block_size);
 
-  if (!length)
-    return;
-
-  if (src != dst)
-    cbc_cts_decrypt_internal(ctx, f, block_size, iv,
-			 length, dst, src);
-  else
+  if (length > block_size)
     {
-      /* We need a copy of the ciphertext, so we can't ECB decrypt in
-       * place.
-       *
-       * If length is small, we allocate a complete copy of src on the
-       * stack. Otherwise, we allocate a block of size at most
-       * CBC_CTS_BUFFER_LIMIT, and process that amount of data at a
-       * time.
-       *
-       * NOTE: We assume that block_size <= CBC_CTS_BUFFER_LIMIT. */
+      nblocks--;
+      if ((length % block_size) == 0)
+	nblocks--;
+    }
 
-      uint8_t *buffer;
+  for ( ; nblocks; nblocks--, src += block_size, dst += block_size)
+    {
+      memcpy(tmpiv, src, block_size);
+      f(ctx, block_size, dst, src);
+      memxor(dst, iv, block_size);
+      memcpy(iv, tmpiv, block_size);
+    }
 
-      if (length <= CBC_CTS_BUFFER_LIMIT)
-	buffer = alloca(length);
-      else
-	{
-	  /* The buffer size must be an integral number of blocks. */
-	  unsigned buffer_size
-	    = CBC_CTS_BUFFER_LIMIT - (CBC_CTS_BUFFER_LIMIT % block_size);
+  if (length > block_size)
+    {
+      memcpy(tmpiv, iv, block_size);
+      memcpy(iv, src + block_size, restbytes);
 
-	  buffer = alloca(buffer_size);
+      f(ctx, block_size, dst, src);
+      memxor (dst, iv, restbytes);
 
-	  for ( ; length >= buffer_size;
-		length -= buffer_size, dst += buffer_size, src += buffer_size)
-	    {
-	      memcpy(buffer, src, buffer_size);
-	      cbc_cts_decrypt_internal(ctx, f, block_size, iv,
-				   buffer_size, dst, buffer);
-	    }
-	  if (!length)
-	    return;
-	}
-      /* Now, we have at most CBC_CTS_BUFFER_LIMIT octets left */
-      memcpy(buffer, src, length);
-
-      cbc_cts_decrypt_internal(ctx, f, block_size, iv,
-			   length, dst, buffer);
+      memcpy(dst + block_size, dst, restbytes);
+      memcpy(iv + restbytes, dst + restbytes, block_size - restbytes);
+      f(ctx, block_size, dst, iv);
+      memxor (dst, tmpiv, block_size);
     }
 }
