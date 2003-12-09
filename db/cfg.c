844dff9ccf64d6bf89236cd125e744f9331e966f
Simon Josefsson <simon@josefsson.org>
2003-12-09 07:56:50 +0000

    Fix error message.

--- db/cfg.c
+++ db/cfg.c
@@ -79,7 +79,10 @@ shisa_cfg_db (Shisa * dbh, char *value)
   rc = backend->init (dbh, location, options, &state);
   if (rc != SHISA_OK)
     {
-      shisa_info (dbh, "Cannot initialize `%s' database backend.", db);
+      shisa_info (dbh, "Cannot initialize `%s' database backend.\n"
+		  "Location `%s' and options `%s'.", db,
+		  location ? location : "N/A",
+		  options ? options : "N/A");
       return rc;
     }
 
