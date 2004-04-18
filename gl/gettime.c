d72e0902ecf247ce4f9d2f8481703cca8c49b539
Simon Josefsson <simon@josefsson.org>
2004-04-18 12:10:19 +0000

    Gnulib sync.

--- gl/gettime.c
+++ gl/gettime.c
@@ -1,7 +1,5 @@
-/* Convert UTC calendar time to simple time.  Like mktime but assumes UTC.
-
-   Copyright (C) 1994, 1997, 2003, 2004 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
+/* gettime -- get the system clock
+   Copyright (C) 2002 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -17,27 +15,32 @@
    along with this program; if not, write to the Free Software Foundation,
    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
+/* Written by Paul Eggert.  */
+
 #ifdef HAVE_CONFIG_H
 # include <config.h>
 #endif
 
-#ifdef _LIBC
-# include <time.h>
-#else
-# include "timegm.h"
-# include <time_r.h>
-# undef __gmtime_r
-# define __gmtime_r gmtime_r
-# define __mktime_internal mktime_internal
-time_t __mktime_internal (struct tm *,
-			  struct tm * (*) (time_t const *, struct tm *),
-			  time_t *);
+#include "timespec.h"
+
+/* Get the system time.  */
+
+int
+gettime (struct timespec *ts)
+{
+#if defined CLOCK_REALTIME && HAVE_CLOCK_GETTIME
+  if (clock_gettime (CLOCK_REALTIME, ts) == 0)
+    return 0;
 #endif
 
-time_t
-timegm (struct tm *tmp)
-{
-  static time_t gmtime_offset;
-  tmp->tm_isdst = 0;
-  return __mktime_internal (tmp, __gmtime_r, &gmtime_offset);
+  {
+    struct timeval tv;
+    int r = gettimeofday (&tv, 0);
+    if (r == 0)
+      {
+	ts->tv_sec = tv.tv_sec;
+	ts->tv_nsec = tv.tv_usec * 1000;
+      }
+    return r;
+  }
 }
--- gl/strdup.h
+++ gl/strdup.h
@@ -1,7 +1,5 @@
-/* Convert UTC calendar time to simple time.  Like mktime but assumes UTC.
-
-   Copyright (C) 1994, 1997, 2003, 2004 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
+/* strdup.h -- duplicate a string
+   Copyright (C) 2004 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -17,27 +15,15 @@
    along with this program; if not, write to the Free Software Foundation,
    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
-#ifdef HAVE_CONFIG_H
-# include <config.h>
+#ifndef STRDUP_H_
+#define STRDUP_H_
+
+/* Get strdup declaration, if available.  */
+#include <string.h>
+
+#if !HAVE_DECL_STRDUP && !defined strdup
+/* Duplicate S, returning an identical malloc'd string.  */
+extern char *strdup (const char *s);
 #endif
 
-#ifdef _LIBC
-# include <time.h>
-#else
-# include "timegm.h"
-# include <time_r.h>
-# undef __gmtime_r
-# define __gmtime_r gmtime_r
-# define __mktime_internal mktime_internal
-time_t __mktime_internal (struct tm *,
-			  struct tm * (*) (time_t const *, struct tm *),
-			  time_t *);
-#endif
-
-time_t
-timegm (struct tm *tmp)
-{
-  static time_t gmtime_offset;
-  tmp->tm_isdst = 0;
-  return __mktime_internal (tmp, __gmtime_r, &gmtime_offset);
-}
+#endif /* STRDUP_H_ */
