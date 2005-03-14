3ed24fc88c1b0d28e0b9cd271fb23e0fad00b50c
Simon Josefsson <simon@josefsson.org>
2005-03-14 14:32:13 +0000

    Sync gnulib.

--- gl/strnlen.h
+++ gl/strnlen.h
@@ -1,5 +1,6 @@
-/* Formatted output to strings.
-   Copyright (C) 1999, 2002 Free Software Foundation, Inc.
+/* Find the length of STRING, but scan at most MAXLEN characters.
+   Copyright (C) 2005 Free Software Foundation, Inc.
+   Written by Simon Josefsson.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -15,28 +16,14 @@
    with this program; if not, write to the Free Software Foundation,
    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
-#ifdef HAVE_CONFIG_H
-# include <config.h>
+#ifndef STRNLEN_H
+#define STRNLEN_H
+
+/* Get strnlen declaration, if available.  */
+#include <string.h>
+
+#if defined HAVE_DECL_STRNLEN && !HAVE_DECL_STRNLEN
+extern size_t strnlen(const char *s, size_t maxlen);
 #endif
 
-/* Specification.  */
-#include "vasprintf.h"
-
-#include <stdlib.h>
-
-#include "vasnprintf.h"
-
-int
-vasprintf (char **resultp, const char *format, va_list args)
-{
-  size_t length;
-  char *result = vasnprintf (NULL, &length, format, args);
-  if (result == NULL)
-    return -1;
-
-  *resultp = result;
-  /* Return the number of resulting bytes, excluding the trailing NUL.
-     If it wouldn't fit in an 'int', vasnprintf() would have returned NULL
-     and set errno to EOVERFLOW.  */
-  return length;
-}
+#endif /* STRNLEN_H */
