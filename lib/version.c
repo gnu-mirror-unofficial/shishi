a3a18106d0d40766485aeb4147542468375fa606
Simon Josefsson <simon@josefsson.org>
2006-10-31 17:19:28 +0000

    Doc fix.

--- lib/version.c
+++ lib/version.c
@@ -3,8 +3,8 @@
  *
  * This file is part of Shishi.
  *
- * Shishi is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * Shishi is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
@@ -14,8 +14,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Shishi; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ * along with Shishi; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
  *
  */
 
