269750b5e0af56452ae2c66c6e356a68fbaa2d12
Simon Josefsson <simon@josefsson.org>
2007-06-27 10:42:26 +0200

    Change GPLv2+ to GPLv3+.

--- examples/client-cksum.c
+++ examples/client-cksum.c
@@ -1,21 +1,22 @@
 /* client-cksum.c --- Sample Shishi authenticated client, with checksum data.
- * Copyright (C) 2003, 2004  Simon Josefsson
+ * Copyright (C) 2003, 2004, 2007  Simon Josefsson
  *
  * This file is part of Shishi.
  *
- * Shishi is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * Shishi is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- * Shishi is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Shishi is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Shishi; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ * along with Shishi; if not, see http://www.gnu.org/licenses or write
+ * to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA
  *
  */
 
