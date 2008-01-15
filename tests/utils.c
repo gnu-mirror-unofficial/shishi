1669fe78cccc2a66878ff8aa9ae14f0c8e05e607
Simon Josefsson <simon@josefsson.org>
2008-01-16 00:40:26 +0100

    Simplify #include's.

--- tests/utils.c
+++ tests/utils.c
@@ -1,5 +1,5 @@
 /* utils.c --- Shishi self tests utilities.
- * Copyright (C) 2002, 2003, 2004, 2006, 2007  Simon Josefsson
+ * Copyright (C) 2002, 2003, 2004, 2006, 2007, 2008  Simon Josefsson
  *
  * This file is part of Shishi.
  *
@@ -34,6 +34,8 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/time.h>
+#include <time.h>
 
 #ifdef HAVE_NETDB_H
 #include <netdb.h>
@@ -55,24 +57,9 @@
 #include <errno.h>
 #endif
 
-#if TIME_WITH_SYS_TIME
-# include <sys/time.h>
-# include <time.h>
-#else
-# if HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
-#endif
-
-#ifdef HAVE_SIGNAL_H
 #include <signal.h>
-#endif
 
-#ifdef HAVE_NETINET_IN_H
 #include <netinet/in.h>
-#endif
 #ifdef HAVE_NETINET_IN6_H
 #include <netinet/in6.h>
 #endif
