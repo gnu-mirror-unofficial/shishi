2d718b5f77ce742be658a7607322487259043de8
Simon Josefsson <simon@josefsson.org>
2008-08-21 10:44:16 +0200

    Update gnulib usage.

--- lib/version.c
+++ lib/version.c
@@ -1,5 +1,5 @@
 /* version.c --- Version handling.
- * Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007  Simon Josefsson
+ * Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008  Simon Josefsson
  *
  * This file is part of Shishi.
  *
@@ -22,7 +22,7 @@
 
 #include "internal.h"
 
-#include <strverscmp.h>
+#include <string.h> /* for strverscmp */
 
 /**
  * shishi_check_version:
