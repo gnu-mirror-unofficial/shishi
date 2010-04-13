ea5765ead933ef0ecde91b759f18683749905e12
Simon Josefsson <simon@josefsson.org>
2010-04-13 10:50:22 +0200

    Fix building with --disable-starttls.
    Reported by Johan van Selst <johans@stack.nl> in
    <http://lists.gnu.org/archive/html/help-shishi/2010-04/msg00000.html>.

--- lib/netio.c
+++ lib/netio.c
@@ -241,9 +241,11 @@ sendrecv_host (Shishi * handle,
 	{
 	  if (transport == TCP)
 	    rc = sendrecv_tcp (handle, ai, indata, inlen, outdata, outlen);
+#ifdef USE_STARTTLS
 	  else if (transport == TLS)
 	    rc = _shishi_sendrecv_tls (handle, ai, indata, inlen,
 				       outdata, outlen);
+#endif
 	  else
 	    rc = sendrecv_udp (handle, ai, indata, inlen, outdata, outlen);
 
